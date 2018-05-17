2ac5be84ce6d0da2f50b0a1a2dc8d2261d7718c2
Dhaval <dhaval.malhotra@hotmail.com>
2018-05-17 13:59:55 -0400

    GitHub test

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,15 +4,31 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.Group;
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.scene.text.Text;
+import javafx.scene.text.Font;
+
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        final Group rootGroup = new Group();
+//        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        final Text text1 = new Text(25, 25, "Quack");
+        text1.setFill(Color.CHOCOLATE);
+        text1.setFont(Font.font(java.awt.Font.SERIF, 25));
+        rootGroup.getChildren().add(text1);
+
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Scene scene1 = new Scene( rootGroup, 300, 275);
+
+        primaryStage.setScene(scene1);
+
+        primaryStage.getIcons().add(new Image("file:" + "C:\\Users\\dhava\\Stuff\\Duck Game\\Icons\\Duck.png"));
         primaryStage.show();
     }
 
