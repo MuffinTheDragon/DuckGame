1e041ecb963675e0e2105ce4670fe7207f172d5d
MuffinTheDragon <dhaval.malhotra@hotmail.com>
2018-05-19 15:03:08 -0400

    Update

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,39 +1,76 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.image.Image;
+import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
-import javafx.stage.Stage;
-import javafx.scene.text.Text;
 import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
 
-public class Main extends Application {
+/*
+
+Stage: Full entire window
+Scene: Content inside your stage. Contains things like buttons.
+Layout: How to arrange everything on the scene
+setCoordinates method: Position every object exactly where you want it to be *
+*/
+
+
+public class Main extends Application { //get functionality for a JavaFX program
+    //to hand user events, implement EventHandler
+    Stage window;
+    Button button;
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
+
         final Group rootGroup = new Group();
+        StackPane layout = new StackPane();
+        Scene scene1 = new Scene(layout, 300, 275); //replace layout with rootGroup to use
+        // rootGroup.getChildren()
 //        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         final Text text1 = new Text(25, 25, "Quack");
         text1.setFill(Color.CHOCOLATE);
         text1.setFont(Font.font(java.awt.Font.SERIF, 25));
-        rootGroup.getChildren().add(text1);
+        layout.getChildren().add(text1);
 
-        primaryStage.setTitle("Hello World");
-        Scene scene1 = new Scene( rootGroup, 300, 275);
+
+        primaryStage.setTitle("Duck Game");
 
         primaryStage.setScene(scene1);
 
-        primaryStage.getIcons().add(new Image("file:" + "C:\\Users\\dhava\\Stuff\\Duck Game\\Icons\\Duck.png"));
+        button = new Button();
+        button.setText("Start");
+        button.setOnAction(e -> System.out.println("lamb")); //Using Lambada expressions for less button code
+
+
+        //button.setOnAction(this) //whenever user clicks the button, look in this class for the handle method
+        // this file
+
+
+        layout.getChildren().add(button);
+
+        primaryStage.getIcons().add(new Image(Main.class.getResourceAsStream("/Images/Duck.png")));
         primaryStage.show();
+
     }
 
-
+    /*
+    @Override
+    public void handle(ActionEvent event) { //this method is called whenever user clicks a button or does an action
+        if(event.getSource() == button) { //identify which button was clicked
+            System.out.println("Nice");
+        }
+    }
+    */
     public static void main(String[] args) {
-        launch(args);
+        launch(args); //method inside application class. Calls start method in Application class
     }
 }
